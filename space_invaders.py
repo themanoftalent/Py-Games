43cda12c3c4af7d408f6cebec36136ff4ebe3244
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-04 13:16:09 +0300

    Create space_invaders.py

---
+++ space_invaders.py
@@ -0,0 +1,46 @@
+import pygame
+
+# create a simple game with pygame
+
+# initialize pygame
+pygame.init()
+
+# create a screen
+screen = pygame.display.set_mode((800, 600))
+
+# title and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+# game loop
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        # if keystroke is pressed check whether it is right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
+    pygame.display.update()
+
+pygame.quit()
